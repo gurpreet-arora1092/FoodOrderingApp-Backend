5df05e359389896252b142aea2079222e3444bfe
Gurpreet Arora <54837248+gurpreet-arora1092@users.noreply.github.com>
2021-03-29 22:04:03 +0530

    Entities, Data Access Object Added in Services

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CategoryItemEntity.java
@@ -0,0 +1,54 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "category_item" , schema = "public")
+public class CategoryItemEntity implements Serializable{
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "item_id")
+    @NotNull
+    private ItemEntity itemId;
+
+    @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "category_id")
+    @NotNull
+    private CategoryEntity categoryId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public ItemEntity getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(ItemEntity itemId) {
+        this.itemId = itemId;
+    }
+
+    public CategoryEntity getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(CategoryEntity categoryId) {
+        this.categoryId = categoryId;
+    }
+}
